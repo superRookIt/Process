1b93b27f928c67799ca976950a38e587280602e4
superRookIt <61442652+superRookIt@users.noreply.github.com>
2020-04-01 17:35:00 +0900

    Add files via upload

---
+++ 4월/jsp_0401_01/WebContent/hello.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<form action="" name="login" method="get">
+
+		<h1>회원가입</h1>
+		<ul>
+			<li><h3>로그인</h3></li>
+			<li><label>아이디</label><input type="text" name="id"></li>
+			<li><label>패스워드</label><input type="password" name="pw"></li>
+			<li><input type="submit" value="전송"><input type="reset" value="취소"></li>
+		</ul>
+	</form>
+
+
+</body>
+</html>
---
+++ 4월/jsp_0401_01/src/com/javalang/ex/Form_ok.java
@@ -0,0 +1,39 @@
+package com.javalang.ex;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/F_ok") // 단축키 [맵핑]
+
+public class Form_ok extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public Form_ok() {
+		super();
+		// 생성자
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// method="get" doGet실행, 주소로 바로 접근해도 실행 [void 메소드]
+		System.out.println("doGet으로 실행됨");
+		actionDo(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// method="post" doPost실행, [void 메소드]
+		System.out.println("doPost으로 실행됨");
+		actionDo(request,response);
+	}
+
+	protected void actionDo(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+}
