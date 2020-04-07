475da46c8dce47faf7588e395f38534d92c846e7
superRookIt <61442652+superRookIt@users.noreply.github.com>
2020-04-07 10:03:43 +0900

    Update Ex0221_01.java

--- 2월/Ex0221/src/Ex0221_01.java
+++ 2월/Ex0221/src/Ex0221_01.java
@@ -1,70 +1,70 @@
-//public class Ex0221_01 {
+public class Ex0221_01 {
+
+	public static void main(String[] args) {
+
+//        Student s = new Student();
+//        
+//        Student t = add(s);
+//    
+
+    
+    static Student add(Student s){
+       return s
+            }
 //
-//	public static void main(String[] args) {
+
+	// Ïù¥Î¶ÑÏù¥ addÎ°ú Í∞ôÏßÄÎßå ÌÉÄÏûÖÏù¥ Îã§Î•¥ÎãàÍπå Í∞ÄÎä•!!
+
+	static void add() {
+	}
+
+	static void add(int a) {
+	}
+	// static void add(int x){} (X) -> ÌÉÄÏûÖÏù¥ Í∞ôÏïÑÏÑú Ïù¥Î¶ÑÏù¥ Îã¨ÎùºÎèÑ ÏÜåÏö©ÏóÜÏùå
+
+	static void add(int a, int b) {
+	}
+	// static void add(int x, int y {} (X) -> ÌÉÄÏûÖÏù¥ Í∞ôÏïÑÏÑú Ïù¥Î¶ÑÏù¥ Îã¨ÎùºÎèÑ ÏÜåÏö©ÏóÜÏùå
+
+	static void add(char a) {
+	}
+
 //
-////        Student s = new Student();
-////        
-////        Student t = add(s);
-////    
-//
-////    
-////    static Student add(Student s){
-////       return s
-////            }
-////
-//
-//	// ¿Ã∏ß¿Ã add∑Œ ∞∞¡ˆ∏∏ ≈∏¿‘¿Ã ¥Ÿ∏£¥œ±Ó ∞°¥…!!
-//
-//	static void add() {
-//	}
-//
-//	static void add(int a) {
-//	}
-//	// static void add(int x){} (X) -> ≈∏¿‘¿Ã ∞∞æ∆º≠ ¿Ã∏ß¿Ã ¥ﬁ∂Ûµµ º“øÎæ¯¿Ω
-//
-//	static void add(int a, int b) {
-//	}
-//	// static void add(int x, int y {} (X) -> ≈∏¿‘¿Ã ∞∞æ∆º≠ ¿Ã∏ß¿Ã ¥ﬁ∂Ûµµ º“øÎæ¯¿Ω
-//
-//	static void add(char a) {
-//	}
-//
-////
-//
-//	int a = 0;
-//	char b = 'a';
-//	String c = "abc";
-//	long d = 1000000000;
-//	float e = 1.5f;
-//	double f = 3.141989788;
-//
-//	print();
-//
-//	print(a);
-//        print(b);
-//        print(c);
-//        print(d);
-// 
-//    }// main
-//
-//	static void print() {
-//		System.out.println("null");
-//	}
-//
-//	static void print(int a) {
-//		System.out.println("int");
-//	}
-//
-//	static void print(char a) {
-//		System.out.println("char");
-//	}
-//
-//	static void print(String a) {
-//		System.out.println("String");
-//	}
-//
-//	static void print(long a) {
-//		System.out.println("long");
-//	}
-//
-//}// class
+
+	int a = 0;
+	char b = 'a';
+	String c = "abc";
+	long d = 1000000000;
+	float e = 1.5f;
+	double f = 3.141989788;
+
+	print();
+
+	print(a);
+        print(b);
+        print(c);
+        print(d);
+ 
+    }// main
+
+	static void print() {
+		System.out.println("null");
+	}
+
+	static void print(int a) {
+		System.out.println("int");
+	}
+
+	static void print(char a) {
+		System.out.println("char");
+	}
+
+	static void print(String a) {
+		System.out.println("String");
+	}
+
+	static void print(long a) {
+		System.out.println("long");
+	}
+
+}// class
