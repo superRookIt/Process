773cefa7e294f09cb70f255debb13689886268f2
superRookIt <61442652+superRookIt@users.noreply.github.com>
2020-04-20 10:03:11 +0900

    Update sql_as.java

--- 4월/jsp_0417/build/classes/com/javalec/ex/sql_as.java
+++ 4월/jsp_0417/build/classes/com/javalec/ex/sql_as.java
@@ -1,18 +1,18 @@
 public ArrayList<DT> member_select() {
 		
-		sql = "select salary*12 as year_s from m2";
-    //as ~~ 일 경우 받아올때 ~~을 입력
+   sql = "select salary*12 as year_s from m2";
+   //as ~~(별칭) 일 경우 받아올때 ~~(별칭)을 입력
 		
 		try {
     
-      c = datasource.getConnection();
-			ps = c.prepareStatement(sql);
-			rs = ps.executeQuery();
+     c = datasource.getConnection();
+     ps = c.prepareStatement(sql);
+     rs = ps.executeQuery();
 
 			
-			while (rs.next()) {
+     while (rs.next()) {
 
-				id = rs.getString("year_s"); 
-        //as ~~ 일 경우 받아올때 ~~을 입력
+      id = rs.getString("year_s"); 
+      //as ~~(별칭) 일 경우 받아올때 ~~(별칭)을 입력
 
-			}
+	}
